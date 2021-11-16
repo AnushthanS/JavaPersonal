30a8640649dfef9f9d38c0dc0277a91725332f68
AnushthanS <anushthans@gmail.com>
2021-11-16 18:46:09 +0530

    backup

---
+++ IdeaProjects/AllRevised/src/Problems/TagContentExtractor.java
@@ -0,0 +1,12 @@
+package Problems;
+
+import static java.lang.System.*;
+import java.util.Scanner;
+import java.util.regex.*;
+
+public class TagContentExtractor {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(in);
+        
+    }
+}
