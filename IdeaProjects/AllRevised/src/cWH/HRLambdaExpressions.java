dec922eb368e602e93c709ec920743bff97a404b
Anushthan Saxena <anushthans@gmail.com>
2021-11-30 02:44:29 +0530

    backup

--- IdeaProjects/AllRevised/src/cWH/HRLambdaExpressions.java
+++ IdeaProjects/AllRevised/src/cWH/HRLambdaExpressions.java
@@ -1,12 +1,10 @@
 package cWH;
 
-import jdk.jfr.Experimental;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.math.BigInteger;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class HRLambdaExpressions {
     public static void main(String[] args) throws IOException {
@@ -15,7 +13,7 @@ public class HRLambdaExpressions {
         int T=Integer.parseInt(br.readLine());
         PerformOperation op;
 
-        boolean ret=false;
+        
         String ans = null;
         while(T-->0){
             String s = br.readLine().trim();
@@ -23,6 +21,7 @@ public class HRLambdaExpressions {
             int ch=Integer.parseInt(st.nextToken());
             int num=Integer.parseInt(st.nextToken());
 
+            boolean ret;
             if(ch==1){
                 op=ob.isOdd();
                 ret =ob.checker(op, num);
@@ -46,17 +45,17 @@ interface PerformOperation{
 }
 
 class MyMath{
-    public static boolean checker(PerformOperation p, int num){
+    public  boolean checker(PerformOperation p, int num){
         return p.check(num);
     }
 
-    public static PerformOperation isOdd(){
+    public  PerformOperation isOdd(){
         return (int a)->a%2!=0;
     }
-    public static PerformOperation isPrime(){
+    public  PerformOperation isPrime(){
         return (int a)-> BigInteger.valueOf(a).isProbablePrime(100);
     }
-    public static PerformOperation isPalindrome(){
+    public  PerformOperation isPalindrome(){
         return (int a)-> Integer.toString(a).equals(new StringBuilder(Integer.toString(a)).reverse().toString());
     }
 //My code here everything else locked
