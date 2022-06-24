34614a21845f456be41fc87d027fcd8ba1f021c2
Anushthan Saxena <anushthans@gmail.com>
2022-06-24 10:08:58 +0530

    backup

--- GUI/src/FrameIntro.java
+++ GUI/src/FrameIntro.java
@@ -1,2 +1,16 @@
+import javax.swing.JFrame;
+import javax.swing.ImageIcon;
+import java.awt.Color;
 public class FrameIntro {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setTitle("This is a JFrame window");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setSize(400, 400);
+        frame.setVisible(true);
+//        ImageIcon image = new ImageIcon("path");
+//        frame.setIconImage(image.getImage());
+        frame.getContentPane().setBackground(new Color(100, 200, 110));
+    }
 }
